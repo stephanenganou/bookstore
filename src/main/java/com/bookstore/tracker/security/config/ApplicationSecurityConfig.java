e4fe1d804400dd2ee7943d10ee0748fb05c8337b
Nganou Wafo, Stephane <stephane.nganou.wafo@ausy-technologies.de>
2021-11-21 22:15:45 +0100

    making all tests work

--- src/main/java/com/bookstore/tracker/security/config/ApplicationSecurityConfig.java
+++ src/main/java/com/bookstore/tracker/security/config/ApplicationSecurityConfig.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -23,6 +24,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
  * @author Stephane Nganou
  * @version 1.0
  */
+@Profile("!test")
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
--- src/test/java/com/bookstore/tracker/controller/book/BookControllerTest.java
+++ src/test/java/com/bookstore/tracker/controller/book/BookControllerTest.java
@@ -1,11 +1,53 @@
 package com.bookstore.tracker.controller.book;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.bookstore.tracker.data.dto.BookDto;
+import com.bookstore.tracker.service.book.BookService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author Stephane Nganou
  * @version 1.0
  */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class BookControllerTest {
 
+    private static final long VALID_BOOK_ID = 11443683;
+    private MockMvc mockMvc;
+
+    @Autowired
+    private BookService bookService;
+    @Autowired
+    private WebApplicationContext context;
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    void whenGetBookById_withExistingBookId_thenAddAttributeToMode() throws Exception {
+        BookDto expectedBookDto = bookService.getBookById(VALID_BOOK_ID);
+        mockMvc.perform(get("/book/{bookId}", VALID_BOOK_ID))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("book", is(expectedBookDto)));
+    }
 }
