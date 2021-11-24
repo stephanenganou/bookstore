42e3ff55132500612d715d986fc58238cdf5a196
Nganou Wafo, Stephane <stephane.nganou.wafo@ausy-technologies.de>
2021-11-24 21:33:59 +0100

    Update

--- src/test/java/com/bookstore/tracker/controller/datamanagement/CSVControllerTest.java
+++ src/test/java/com/bookstore/tracker/controller/datamanagement/CSVControllerTest.java
@@ -1,8 +1,13 @@
 package com.bookstore.tracker.controller.datamanagement;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 /**
  * @author Stephane Nganou
  * @version 1.0
  */
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CSVControllerTest {
 }
